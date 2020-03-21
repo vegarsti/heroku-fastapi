0ac8d16aafe28667fee8579522c0f0b3df3b7189
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2020-03-21 15:25:57 +0100

    Simplify app

--- sql_app/main.py
+++ sql_app/main.py
@@ -20,6 +20,11 @@ def get_db():
         db.close()
 
 
+@app.get("")
+def main():
+    return {"Hi Daniel!"}
+
+
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
@@ -40,16 +45,3 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
-
-
-@app.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(
-    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
-
-
-@app.get("/items/", response_model=List[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,23 +1,4 @@
-from typing import List
-
 from pydantic import BaseModel
-
-
-class ItemBase(BaseModel):
-    title: str
-    description: str = ""
-
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
-    id: int
-    owner_id: int
-
-    class Config:
-        orm_mode = True
 
 
 class UserBase(BaseModel):
@@ -31,7 +12,6 @@ class UserCreate(UserBase):
 class User(UserBase):
     id: int
     is_active: bool
-    items: List[Item] = []
 
     class Config:
         orm_mode = True
